08757b369eeabbe7997c121bd803202cb1216b95
Leandro Medus <leomedus@gmail.com>
2017-09-13 16:53:42 -0300

    Se definen los métodos y atributos de la clase Imagen, quedando pendiente la implementación de los mismos.

--- imagen.cpp
+++ imagen.cpp
@@ -1,6 +1,79 @@
 #include "imagen.h"
+#include "pixel.h"
 
 Imagen::Imagen()
+{
+
+}
+
+/* ----------------------- Setters ----------------------- */
+void Imagen::setXSizePx(unsigned int value)
+{
+    xSizePx = value;
+}
+
+void Imagen::setYSizePx(unsigned int value)
+{
+    ySizePx = value;
+}
+
+void Imagen::setXSize(unsigned int value)
+{
+    xSize = value;
+}
+
+void Imagen::setYSize(unsigned int value)
+{
+    ySize = value;
+}
+
+void Imagen::setUnitOfMeasurement(const string &value)
+{
+    unitOfMeasurement = value;
+}
+
+/* ----------------------- Getters ----------------------- */
+unsigned int Imagen::getXSizePx() const
+{
+    return xSizePx;
+}
+
+unsigned int Imagen::getYSizePx() const
+{
+    return ySizePx;
+}
+
+unsigned int Imagen::getXSize() const
+{
+    return xSize;
+}
+
+unsigned int Imagen::getYSize() const
+{
+    return ySize;
+}
+
+string Imagen::getUnitOfMeasurement() const
+{
+    return unitOfMeasurement;
+}
+
+
+/* ------------ Public methods to manipulate an image ------------ */
+
+void Imagen::setPixel(unsigned int x, unsigned int y, const Pixel &dataPixel)
+{
+
+}
+
+Pixel *Imagen::getPixel(unsigned int x, unsigned int y) const
+{
+
+}
+
+/* ------------------ Sobrecarga de operadores ------------------- */
+
+Pixel *Imagen::operator()(unsigned int x, unsigned int y)
 {
 
 }
--- imagen.h
+++ imagen.h
@@ -1,13 +1,88 @@
 #ifndef IMAGEN_H
 #define IMAGEN_H
 
+#include <vector>
+#include <string>
+#include "pixel.h"
+#include "metadatos.h"
+
+using std::vector;
+using std::string;
+
 /*!
  * \brief Clase que modela una imagen
+ * Esta clase modela toda la información contenida en un archivo de
+ * imagen. Por lo tanto contendrá los atributos de la misma (resolución
+ * en x, resolución en y, ancho, alto y dimensión física), los
+ * metadatos y una matriz correspondiente a los pixeles.
  */
 class Imagen
 {
+private:
+    /* Atributos de la imagen */
+    /*! Cantidad de píxeles en el eje X de la imagen */
+    unsigned int xSizePx;
+    /*! Cantidad de píxeles en el eje Y de la imagen */
+    unsigned int ySizePx;
+    /*! Dimensión física de la imagen en X */
+    unsigned int xSize;
+    /*! Dimensión física de la imagen en Y */
+    unsigned int ySize;
+    /*! Unidad físca de medida empleada para medir la imagen */
+    string unitOfMeasurement;
+
+    /* Metadatos de la imagen */
+    Metadatos metadatos;
+
+    /* pixeles de la imagen en formato mxn */
+    vector<vector<Pixel>> pixels;
+
 public:
+    /*! Constructor vacío de la clase Imagen */
     Imagen();
+
+    /* Setters */
+    void setXSizePx(unsigned int value);
+    void setYSizePx(unsigned int value);
+    void setXSize(unsigned int value);
+    void setYSize(unsigned int value);
+    void setUnitOfMeasurement(const string &value);
+
+    /* Getters */
+    unsigned int getXSizePx() const;
+    unsigned int getYSizePx() const;
+    unsigned int getXSize() const;
+    unsigned int getYSize() const;
+    string getUnitOfMeasurement() const;
+
+    /* Public methods to manipulate an image */
+    /*!
+     * \brief Método para setear un valor de un pixel en la imagen
+     * \param x: coordenada en x [px]
+     * \param y: coordenada en y [px]
+     * \param dataPixel: objeto tipo Pixel con la información a guardar
+     */
+    void setPixel(unsigned int x, unsigned int y, const Pixel &dataPixel);
+    /*!
+     * \brief getPixel
+     * \param x: coordenada en x [px]
+     * \param y: coordenada en y [px]
+     * \return puntero a un objeto tipo Pixel de la imagen.
+     */
+    Pixel * getPixel(unsigned int x, unsigned int y) const;
+
+    /* Sobrecarga de operadores */
+    /*!
+     * \brief operator () para acceder a un pixel mediante sus
+     * coordenadas x e y, y a su vez modificarlo. Esto permitiría
+     * modificar el brillo de un pixel como:
+     * imagen(x,y) = imagen(x,y)+1; si la suma estaría definida.
+     * \param x: coordenada en x [px]
+     * \param y: coordenada en y [px]
+     * \return puntero al pixel.
+     */
+    Pixel * operator()(unsigned int x, unsigned int y);
+
 };
 
 #endif // IMAGEN_H
